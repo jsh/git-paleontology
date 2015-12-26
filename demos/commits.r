9cd2188a93dcb47d4dc45f461a13642636110694
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2015-12-26 13:02:11 -0700

    clean up R graphing

--- demos/commits.r
+++ demos/commits.r
@@ -1,7 +1,6 @@
-#!/usr/local/bin/r
 cc <- read.csv("total-commits.dat", sep="")
 plot(cc, main="Cumulative Commits")
-fit <- lm(cc$total ~ cc$week)
+fit <- lm(cc$commits ~ cc$week)
 abline(fit, col="red", lwd=4)
-cor(cc$week, cc$total)
+cor(cc$week, cc$commits)
 fit
